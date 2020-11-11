2ad4654e8f4196717013c653b8762992ae59ee3a
Marcelo Terenciani <marcelo.terenciani@gmail.com>
2020-11-10 22:03:05 -0300

    Barra de navegação - somente HTML

--- src/componentes/BarraNavegacao/BarraNavegacao.js
+++ src/componentes/BarraNavegacao/BarraNavegacao.js
@@ -1,39 +1,33 @@
 
 import './estilo.css';
+import logo from './../../assets/logo-amaer.png'
 import React, { Component } from 'react';
-import {Navbar, Nav, Form, FormControl, Button, NavDropdown} from 'react-bootstrap';
 
 
 class BarraNavegacao extends Component{
-  constructor(props){
-    super(props);
-  }
 
   render(){
-    return (
-      <Navbar bg="dark" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
-  )
-
+    return <nav className="navbar navbar-expand-lg navbar-dark py-2 bg-dark nav-estilo" id="menuopcoes">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img src={logo} className="d-inline-block align-top" alt="" height="50" />
+        </a>
+        <button className="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar17">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbar17">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item my-3 text-light flex-grow-0" > <a className="nav-link" href="index.html">INÍCIO</a> </li>
+            <li className="nav-item py-3 mx-auto"> <a className="nav-link" href="index.html#sobrenos">SOBRE NÓS</a> </li>
+            <li className="nav-item py-3 mx-2"> <a className="nav-link" href="evento.html">EVENTOS</a> </li>
+            <li className="nav-item py-3 mx-1"> <a className="nav-link" href="fotosevideos.html">&nbsp;FOTOS E VÍDEOS</a> </li>
+            <li className="nav-item py-3 mx-1"> <a className="nav-link" href="produtos.html">CLASSIFICADOS</a> </li>
+            <li className="nav-item py-3 mx-1" > <a className="nav-link" href="index.html#contato">CONTATO<br /></a> </li>
+          </ul>
+          <a className="btn navbar-btn ml-md-2 btn-outline-light" href="login.html">Log in</a><a className="btn btn-outline-light navbar-btn ml-md-2" href="cadastro.html">Associe-se</a>
+        </div>
+      </div>
+    </nav>
   }
 }
 
