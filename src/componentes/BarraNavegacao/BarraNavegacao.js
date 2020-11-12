af5d124736dee4451d4ba71658912ea4d23e8131
raquelsantosoliveira <raquel.dossantos1965@gmail.com>
2020-11-11 21:01:29 -0300

    Criação da barra de navegação

--- src/componentes/BarraNavegacao/BarraNavegacao.js
+++ src/componentes/BarraNavegacao/BarraNavegacao.js
@@ -1,38 +1,47 @@
 
 import './estilo.css';
 import React, { Component } from 'react';
-import {Navbar, Nav, Form, FormControl, Button, NavDropdown} from 'react-bootstrap';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import logo from './../../assets/logo.jpeg';
 
 
 class BarraNavegacao extends Component{
   constructor(props){
     super(props);
   }
+     
 
   render(){
     return (
-      <Navbar bg="dark" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
-  )
+        <Navbar  className = "nav-texto-branco" expand = "lg">
+          <Container> 
+            <Navbar.Brand href="#home">
+              <img
+                src= {logo}
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">INÍCIO</Nav.Link>
+                <Nav.Link href="#link">SOBRE NÓS</Nav.Link>
+                <Nav.Link href="#link">EVENTOS</Nav.Link>
+                <Nav.Link href="#link">FOTOS E VÍDEOS</Nav.Link>
+                <Nav.Link href="#link">CLASSIFICADOS</Nav.Link>
+                <Nav.Link href="#link">CONTATO</Nav.Link>
+               </Nav>
+               <Nav>
+                <Nav.Link href="#link" className="ml-md-2 btn btn-outline-light">Associe-se</Nav.Link>
+                <Nav.Link href="#link" className="ml-md-2 mt-md-2 mt-lg-0 btn btn-outline-light">Área de Associado</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+    )
 
   }
 }
