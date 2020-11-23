585619561a1475594cb610e20868e23627dd652f
raquelsantosoliveira <raquel.dossantos1965@gmail.com>
2020-11-23 18:00:42 -0300

    Contrução do formulário de cadastro

--- src/paginas/Associar/Associar.js
+++ src/paginas/Associar/Associar.js
@@ -21,9 +21,63 @@ class Associar extends Component{
           <Form className = "pb-5 formulario-contato" onSubmit={this.handleSubmit}>
             <Row >
               <Col>
-                <Form.Control  className="mb-5" placeholder="Nome" id="nome" name="nome" size="lg"  />
+                <Form.File className="mb-3" id="formcheck-api-regular">
+                <Form.File.Label>Imagem</Form.File.Label>
+                <Form.File.Input />
+                </Form.File>
+                <Form.Control  className="mb-5" placeholder="Nome de Usuário" id="usuario" name="usuario" size="lg"  />
+                <Form.Control  className="mb-5" placeholder="Nome Completo" id="nome" name="nome" size="lg"  />
+                <Form.Control className="mb-5" type="date" placeholder="RG" id="dataNasc" name="dataNasc" size="lg" />
                 <Form.Control className="mb-5"  placeholder="CPF" id="cpf" name="cpf" size="lg" />
-                <Form.Control className="mb-5"  placeholder="RG" id="rg" name="rg" size="lg" />
+                <Form.Control className="mb-5"  placeholder="RG" id="rg" name="rg" size="lg" />  
+                <Row>
+                  <Col>
+                    <Form.Control  className="mb-5" placeholder="Rua" id="rua" name="rua" size="lg"  />
+                  </Col>
+                  <Col>
+                    <Form.Control  className="mb-5" placeholder="Número" id="numero" name="numero" size="lg"  />
+                  </Col>
+                  <Form.Control  className="mb-5" placeholder="Cidade" id="cidade" name="cidade" size="lg"  />
+                  <Form.Control  className="mb-5" placeholder="Estado" id="estado" name="estado" size="lg"  />
+                  <Form.Control  className="mb-5" placeholder="CEP" id="cep" name="cep" size="lg"  />
+                </Row>   
+                <Form.Control  className="mb-5" placeholder="Telefone Residencial" id="telresi" name="telresi" size="lg"  />     
+                <Form.Control  className="mb-5" placeholder="Telefone Celular" id="telcel" name="telcel" size="lg"  />   
+                <Form.Control  className="mb-5" placeholder="Telefone Comercial" id="telcom" name="telcom" size="lg"  />  
+                <Form.Label>
+                  Celular possui WhatsApp?
+                </Form.Label > 
+                <Form.Check className="mb-5" type="checkbox" label="Possui" />
+                <Form.Control  className="mb-5" placeholder="E-mail" id="email" name="email" size="lg"  /> 
+                <Form.Control  className="mb-5" placeholder="E-mal alternativo" id="emailalter" name="emailalter" size="lg"  />   
+                <Form.Label>
+                  Modalidade Principal:
+                </Form.Label > 
+                <Form.Check className="mb-2" type="checkbox" label="Aeromodelismo" />
+                <Form.Check className="mb-2" type="checkbox" label="Automodelismo" />
+                <Form.Label>
+                  Quantidade de dependentes 
+                </Form.Label > 
+                <Form.Control className="mb-3" as="select" custom>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                </Form.Control>
+                <Form.Control  className="mb-5" placeholder="Nome dos Dependentes" id="dependentes" name="dependentes" size="lg"  />   
+                <Form.Label>
+                  Aceito receber comunicados oficiais oriundos da diretoria da Amaer 
+                </Form.Label > 
+                <Form.Check className="mb-5" type="checkbox" label="Concordo" />
+                <Row>
+                  <Col>
+                    <Form.Control  className="mb-5" type="password" placeholder="Senha" id="senha" name="senha" size="lg"  /> 
+                  </Col>
+                  <Col>
+                    <Form.Control  className="mb-5" type="password" placeholder="Confirmar Senha" id="confirmarsenha" name="confirmarsenha" size="lg"  /> 
+                  </Col>
+                </Row>
                 <Button block size="lg" variant="primary" type="submit" >
                     Enviar
                 </Button>
