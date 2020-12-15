363f3bda72771a82f9c6571b6185457bd81f1aa5
raquelsantosoliveira <raquel.dossantos1965@gmail.com>
2020-12-14 21:39:52 -0300

    Criação da página eventos

--- src/paginas/Eventos/Eventos.js
+++ src/paginas/Eventos/Eventos.js
@@ -1,11 +1,52 @@
 import './estilo.css';
-import React, { Component } from 'react';
+import React, { Component, Card, Row, Button, CardDeck, Container} from 'react';
+import EventoService from './../../service/EventoService'
+// import Eventos from './../../componentes/Eventos/Eventos';
+
 
 
 class Eventos extends Component{
+  constructor (props){
+    super(props);
+    this.state = {
+        eventos: []
+    }
+  }
+  async componentDidMount() {
+      let eventos = await EventoService.obterEventos();
+      this.setState({ eventos })
+  }
   render(){
+    const {eventos} = this.state
+    // let eventos2 = this.state.eventos;
     return (
-        <div>Rota para Eventos</div>
+      <Container>
+        <Row className="justify-content-center">
+          <h1 className="mb-3 mt-3 text-dark text-xs-center">Eventos</h1>
+        </Row>
+        <CardDeck className="mb-3">
+          {
+            eventos.map(evento => {
+              return (
+                <Card className = "col-6 col-lg-4 p-4 my-3 mx-4 borda-cards-eventos">
+                  <Card.Img variant="top" 
+                    src= {evento.src}
+                    alt={evento.alt}
+                  />
+                  <Card.Body>
+                    <Card.Title>{evento.titulo} </Card.Title>
+                    <Card.Text>{evento.descricao} </Card.Text>
+                    <Card.Link>{evento.link}</Card.Link>
+                  </Card.Body>
+                </Card>
+              )}
+            )
+          }
+        </CardDeck>
+        <Row className="justify-content-end">
+          <Button href="/eventos" variant="secondary" size="lg">+ Eventos</Button>
+        </Row>
+      </Container>   
     )
 
   }
---
+++ src/service/EventoService.js
@@ -0,0 +1,37 @@
+
+
+class Eventos{
+    
+    static obterEventos(){
+        const eventos = [
+            {
+                id:1,
+                src: "https://i0.wp.com/www.amaer.com.br/wp-content/uploads/2019/09/amaer-campeonato-banner-1.jpg?w=1200&ssl=1",
+                alt: "First slide",
+                titulo: "Notícia 1",
+                descricao: "Informações Notícia 1.",
+                link: "https://google.com"
+            },
+            {
+                id:2,
+                src: "https://i0.wp.com/www.amaer.com.br/wp-content/uploads/2019/09/amaer-campeonato-banner-1.jpg?w=1200&ssl=1",
+                alt: "First slide",
+                titulo: "Notícia 2",
+                descricao: "Informações Notícia 2.",
+                link: "https://google.com"
+            },
+            {
+                id:3,
+                src: "https://i0.wp.com/www.amaer.com.br/wp-content/uploads/2019/09/amaer-campeonato-banner-1.jpg?w=1200&ssl=1",
+                alt: "First slide",
+                titulo: "Notícia 3",
+                descricao: "Informações Notícia 3.",
+                link: "https://google.com"
+            }
+        ]
+        return eventos;
+    }
+    
+}
+
+export default Eventos;
