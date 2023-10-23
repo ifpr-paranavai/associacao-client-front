668578e6dd4715915c18c76ee052a38e514ee5c0
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-10-23 00:28:45 -0300

    Exibição do video

--- src/paginas/FotosVideos/FotosVideos.js
+++ src/paginas/FotosVideos/FotosVideos.js
@@ -1,74 +1,236 @@
-import './estilo.css';
-import React, { Component} from 'react';
-import {Card, Row, Button, CardDeck, Container} from 'react-bootstrap';
-import FotosVideosService from '../../service/FotosVideosService'
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Paper,
+  Card,
+  Grid,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Avatar,
+  IconButton,
+  Container,
+  Button,
+  TableRow,
+  TableCell,
+  TablePagination,
+  LinearProgress,
+  InputAdornment,
+  Dialog,
+  DialogContent,
+} from "@material-ui/core";
+import { Row, Button as ButtonBootstrap } from "react-bootstrap";
+import { formatarData } from "../../uteis/formatarData";
+import VideoService from "../../service/VideosService";
+import { useNotify } from "../../contextos/Notificacao";
+import styles from "./estilo.css";
+import { Link } from "react-router-dom";
+import ReactQuill from "react-quill";
+import { Search as SearchIcon } from "@material-ui/icons";
+import API from "../../Api";
+import TextField from "@material-ui/core/TextField";
+import YouTube from "react-youtube";
+import CloseIcon from "@material-ui/icons/Close";
 
+function Videos() {
+  const [videos, setVideos] = useState([]);
+  const notify = useNotify();
+  const [searchValue, setSearchValue] = useState("");
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(3);
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedVideoId, setSelectedVideoId] = useState(null);
 
+  const openModal = (videoId) => {
+    setSelectedVideoId(videoId);
+    setModalOpen(true);
+  };
 
-class FotosVideos extends Component{
-  constructor (props){
-    super(props);
-    this.state = {
-        fotos: [],
-        videos: []
+  const closeModal = () => {
+    setSelectedVideoId(null);
+    setModalOpen(false);
+  };
 
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+    setPage(0);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        let dadosAPI;
+        if (searchValue) {
+          dadosAPI = await VideoService.buscarPorTitulo(
+            searchValue,
+            rowsPerPage,
+            page + 1
+          );
+        } else {
+          dadosAPI = await VideoService.listarVideos(rowsPerPage, page + 1);
+        }
+        setCount(dadosAPI.count || dadosAPI.length);
+        setVideos(dadosAPI.rows || dadosAPI);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
     }
+    fetchData();
+  }, [searchValue, page, rowsPerPage]);
+
+  function extractVideoId(url) {
+    const regex =
+      /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
   }
-  async componentDidMount() {
-      let fotos = await FotosVideosService.obterFotos();
-      this.setState({ fotos })
- 
-      let videos = await FotosVideosService.obterVideos();
-      this.setState({ videos })
-}
-   render(){
-   
-    const {fotos} = this.state
-    const {videos} = this.state
-    return (
-      <Container className="py-5">
-        <Row className="justify-content-center">
-          <h1 className="mb-3 mt-3 text-dark text-xs-center">Fotos</h1>
-        </Row>
-        <Row className="justify-content-end">
-          <Button href="/classificados" variant="secondary" size="lg">+ Fotos</Button>
-        </Row>
-        <CardDeck className="mb-3">
-          {
-            fotos.length > 0 && fotos.map(foto => {
-              return (
-                <Card className = "col-6 col-lg-4 p-4 my-3 mx-4 borda-cards-eventos" key={foto.id}>
-                  <Card.Img variant="top" 
-                    src= {foto.src }
-                    alt={foto.alt}
-                  />
-                </Card>
-              )}
-            )
+
+  return (
+    <Container>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        paddingBottom="100px"
+        paddingTop="12px"
+      ></Box>
+      <Row className="justify-content-center">
+        <h1 className="mb-3 mt-3 text-dark text-xs-center">Videos</h1>
+      </Row>
+      <TextField
+        placeholder="Buscar por titulo"
+        variant="outlined"
+        size="small"
+        style={{ width: "100%", maxWidth: "400px", padding: "15px" }}
+        value={searchValue}
+        onChange={handleSearchChange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon color="action" />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Grid container spacing={3}>
+        {(() => {
+          if (loading) {
+            return (
+              <TableRow>
+                <TableCell colSpan={3} align="center">
+                  <LinearProgress />
+                </TableCell>
+              </TableRow>
+            );
           }
-        </CardDeck>
-        <Row className="justify-content-center">
-          <h1 className="mb-3 mt-3 text-dark text-xs-center">Vídeos</h1>
-        </Row>
-        <CardDeck className="mb-3">
-          {
-            videos.length > 0 && videos.map(video => {
+          if (videos.length > 0) {
+            return videos.map((video) => {
+              const videoId = extractVideoId(video.link);
+              const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+
               return (
-                <Card className = "col-6 col-lg-4 p-4 my-3 mx-4 borda-cards-eventos" key={video.id}>
-                  <Card.Img variant="top" 
-                    src= {video.src }
-                    alt={video.alt}
-                  />
-                </Card>
-              )}
-            )
+                <Grid item key={video.id} xs={12} sm={6} md={4}>
+                  <Card
+                    style={{
+                      borderRadius: "16px",
+                      position: "relative",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      alt="Thumbnail do Video"
+                      height="220"
+                      image={thumbnailUrl}
+                      title="Thumbnail do Video"
+                      onClick={() => openModal(videoId)}
+                    />
+                    <CardContent>
+                      <h2
+                        style={{
+                          background: "none",
+                          border: "none",
+                          padding: 0,
+                          fontFamily: "Arial",
+                          wordWrap: "break-word",
+                          color: "black",
+                          cursor: "pointer",
+                          transition: "color 0.2s ease",
+                        }}
+                        onClick={() => openModal(videoId)}
+                        onMouseEnter={(e) => {
+                          e.target.style.color = "blue";
+                        }}
+                        onMouseLeave={(e) => {
+                          e.target.style.color = "black";
+                        }}
+                      >
+                        {video.titulo}
+                      </h2>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            });
           }
-        </CardDeck>
-          <Row className="justify-content-end">
-            <Button href="/fotosvideos" variant="secondary" size="lg">+ Vídeos</Button>
-          </Row>
-      </Container>
-    )
-  }
+          return (
+            <Grid>
+              <TableCell colSpan={3} align="center">
+                Nenhuma ata encontrada
+              </TableCell>
+            </Grid>
+          );
+        })()}
+      </Grid>
+      <TablePagination
+        rowsPerPageOptions={[3, 6, 12, 24]}
+        component="div"
+        count={count}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={(event, newPage) => setPage(newPage)}
+        onRowsPerPageChange={(event) => {
+          setRowsPerPage(parseInt(event.target.value, 10));
+          setPage(0);
+        }}
+        disabled={loading}
+      />
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        paddingBottom="20px"
+        paddingTop="12px"
+      ></Box>
+      <Dialog open={modalOpen} onClose={closeModal} maxWidth="lg" scroll="body">
+      <IconButton
+        edge="end"
+        color="inherit"
+        onClick={closeModal}
+        style={{ position: "absolute", right: 0, top: 0, zIndex: 1 }}
+      >
+        <CloseIcon />
+      </IconButton>
+        <DialogContent>
+          {selectedVideoId && (
+            <YouTube
+              videoId={selectedVideoId}
+              opts={{ width: "100%", height: "540px", width: "960px" }}
+            />
+          )}
+        </DialogContent>
+      </Dialog>
+      ;
+    </Container>
+  );
 }
-export default FotosVideos;
+
+export default Videos;
