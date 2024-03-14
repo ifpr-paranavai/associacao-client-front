483f22ec4d9611c1d3f45b2bc90fffc19117dd21
netorapg <renatoplatz11@outlook.com>
2024-03-14 12:12:56 -0300

    Trabalhando nas modificações da tela inicial. Primeiramente eu retirei o efeito blur, que além de ser brega, causava um lag na página. O professor Frank sugeriu remover a imagem do Aeromodelo de fundo, mas decidi não fazer isso, ao invés disso, adicionei um filtro opaco para escurecê-la e com isso, dar mais destaque no texto "Sobre nós". Apesar de ter dado mais elegância na página na versão web, na mobile a imagem fica um tanto esquisita, irei trabalhar em meios para tornar mais agradável.

--- src/componentes/SobreNos/SobreNos.js
+++ src/componentes/SobreNos/SobreNos.js
@@ -10,15 +10,8 @@ class SobreNos extends Component {
       <Container className="text-white h-100">
         <Row className="h-100 align-items-center">
           <Col className="px-lg-5 d-flex flex-column justify-content-center shadow-lg">
-            <h1 className="text-center">{principal.titulo}</h1>
+            <h1 className="text-left">{principal.titulo}</h1>
             <p className="mb-3 lead">{principal.subtitulo}</p>
-          </Col>
-          <Col>
-            <img
-              className="img-fluid d-block "
-              src={apiHost + "/imagem/" + logo.src}
-              alt={logo.alt}
-            />
           </Col>
         </Row>
       </Container>
